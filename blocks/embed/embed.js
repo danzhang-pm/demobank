0202785c68a3a25b8c5d2cf7e8f381b4745723b7
danzhan-adobe-com <danzhan@adobe.com>
2023-09-21 12:53:15 +0800

    fix the style of bilibili box

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -50,9 +50,11 @@ const loadScript = (url, callback, type) => {
   };
   
   const embedBilibili = (url, autoplay) => {
-    const embedHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
+    const embedHTML = `<div>
         <iframe src="${url.href}" 
-        scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>
+        width="100%" height="500" 
+        scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"
+        </iframe>
       </div>`;
     return embedHTML;
   };
